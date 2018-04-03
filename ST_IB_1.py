5becc9606575289a3a555f2fd6fde1a60335043a
Alexander Hohl <34194710+alexandster@users.noreply.github.com>
2018-04-03 18:03:25 -0400

    Update ST_IB_1.py

--- ST_IB_1.py
+++ ST_IB_1.py
@@ -25,43 +25,61 @@ sTree = spatial.cKDTree(inXYT_s[:,:2])
 stNeighIndex = []    #stores indexes of spatiotemporal neighbors
 
 mNN = np.inf        #minimum number of neighbors
-NN = 2000            #number of nearest neighbors to search for
+NN = 4              #desired number of nearest neighbors
+bN = 2000           #big number
 i = 0              # iterator variable
 
 #loop through all data points
-while i < numPts:
-# while i < 300:
+##while i < numPts:
+while i < 30:
 
     #query point
     sCoord = inXYT_s[i,:2]
     tCoord = inXYT_s[i,2]
 
     #query the tree, input number of neighbors to search for
-    sNeigh = sTree.query(sCoord,NN)
+    sNeigh = sTree.query(sCoord,bN)
 
+    
     #search for temporal neighbors
     tNeigh = []
-    #number of temporal neighbors
-    if i < NN:
+    
+    if i < bN:
         current = 0
     else:
-        current = i - NN
+        current = i - bN
 
+    #if temporal coordinate of neighbor smaller than temporal coordinate of current point, append to list    
     while inXYT_s[current,2] < inXYT_s[i,2]:
         tNeigh.append(current)
         current += 1
 
-    #intersect spatial and temporal neighbors
-    stNeigh = np.intersect1d(list(sNeigh[1]), tNeigh)  #indices
+    print("tNeigh", tNeigh)
 
-    #write to file
-    s = ", ".join(map(str, list(stNeigh)))
-    outFile.write(str(i) + "," + str(sCoord[0]) + "," + str(sCoord[1]) + "," + str(tCoord) + "," + s + "\n")
+    #keep intersecting spatial and temporal neighbor lists until desired number of ST neighbors is reached
+    stNeighLen = 0
+    count = 5
+    iteration = 0
 
-    #keep track of minimum number of spatiotemporal neighbors
-    nSTN = stNeigh.size #number of space-time neighbors
-    if nSTN < mNN:
-        mNN = nSTN
+    if len(tNeigh) > NN:
+    
+        while stNeighLen < NN and iteration < 100:
+            #intersect spatial and temporal neighbors
+            stNeigh = np.intersect1d(list(sNeigh[1][:count]), tNeigh[:count])  #indices
+            stNeighLen = len(stNeigh)
+            print("stNeighLen", stNeighLen)
+            count += 1
+            iteration += 1
+##        
+##
+##    #write to file
+##    s = ", ".join(map(str, list(stNeigh)))
+##    outFile.write(str(i) + "," + str(sCoord[0]) + "," + str(sCoord[1]) + "," + str(tCoord) + "," + s + "\n")
+##
+##    #keep track of minimum number of spatiotemporal neighbors
+##    nSTN = stNeigh.size #number of space-time neighbors
+##    if nSTN < mNN:
+##        mNN = nSTN
 
     i += 1
 
